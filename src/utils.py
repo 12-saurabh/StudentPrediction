979f9242a483b187928a0837e011a3bd11a97b3b
saurabh <kardamsk123@gmail.com>
2026-01-24 14:59:24 +0530

    Deployement configuration for AWS

--- src/utils.py
+++ src/utils.py
@@ -1,65 +1,62 @@
-import sys
 import os
-import numpy as np
-import pandas as pd
+import sys
 
+import numpy as np 
+import pandas as pd
 import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-from sklearn.metrics import r2_score
-
-from sklearn.model_selection import GridSearchCV
-from catboost import CatBoostRegressor
-
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
-        
-        os.makedirs(dir_path,exist_ok=True)
-        
-        with open(file_path,"wb")as file_obj:
-            dill.dump(obj,file_obj)
-            
-    except Exception as e:
-        raise CustomException(e,sys)
-    
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
-    try:
-        report = {}
-        trained_models = {}
+        dir_path = os.path.dirname(file_path)
 
-        for model_name, model in models.items():
+        os.makedirs(dir_path, exist_ok=True)
 
-            if model_name == "CatBoostRegressor":
-                model.fit(X_train, y_train)
-                y_pred = model.predict(X_test)
-                score = r2_score(y_test, y_pred)
-
-                report[model_name] = score
-                trained_models[model_name] = model
-                continue
-
-            gs = GridSearchCV(
-                model,
-                param[model_name],
-                cv=3
-            )
-
-            gs.fit(X_train, y_train)
-
-            best_model = gs.best_estimator_
-            best_model.fit(X_train, y_train)
-
-            y_pred = best_model.predict(X_test)
-            score = r2_score(y_test, y_pred)
-
-            report[model_name] = score
-            trained_models[model_name] = best_model
-
-        return report, trained_models
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
-
     
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
